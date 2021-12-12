1b0bc50cfb5be3742465d52305db64af7cc35efe
Diego <diegoac053@gmail.com>
2021-12-11 20:01:45 -0600

    Added security and swagger

--- src/main/java/Kodigo/challenge/SecurityConfig.java
+++ src/main/java/Kodigo/challenge/SecurityConfig.java
@@ -1,41 +1,80 @@
 package Kodigo.challenge;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+    };
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN")
+                .and()
+                .withUser("diego").password(passwordEncoder().encode("1234go")).roles("USER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("1564")).roles("CLIENT");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.httpBasic();
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers(SWAGGER_WHITELIST).permitAll()
+                .antMatchers(HttpMethod.GET).authenticated()
+                .antMatchers(HttpMethod.PUT).authenticated()
+                .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST).hasAnyRole("ADMIN", "USER")
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+            response.setContentType("application/json;charset=UTF-8");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            try {
+                response.getWriter().write(new JSONObject()
+                        .put("message", "The user name and password combination is incorrect.")
+                        .put("statusCode: ", "403")
+                        .toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+        http.exceptionHandling().accessDeniedHandler((request, response, accessDeniedException) -> {
+            response.setContentType("application/json;charset=UTF-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            try {
+                response.getWriter().write(new JSONObject()
+                        .put("message", "This user does not have the sufficient level of access.")
+                        .put("statusCode: ", "401")
+                        .toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
